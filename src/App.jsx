28497cf9cea95e68a6ec97c8f941a6ef2b8cf1d9
Rahul Kumar <cse.170310430@silicon.ac.in>
2024-02-10 21:32:35 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import AppHeader from "./components/Header/AppHeader";
+
+function App() {
+  return (
+    <Fragment>
+      <AppHeader />
+    </Fragment>
+  );
+}
+
+export default App;
